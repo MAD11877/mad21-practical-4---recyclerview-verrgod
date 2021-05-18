db755e687b4997a13bbfb4c9ea3ee3415735042d
verrgod <lightsmitevernon@gmail.com>
2021-05-18 21:14:12 +0800

    Complete practical 4

--- app/src/main/java/sg/edu/np/mad/practical2/UserViewHolder.java
+++ app/src/main/java/sg/edu/np/mad/practical2/UserViewHolder.java
@@ -10,7 +10,9 @@ public class UserViewHolder extends RecyclerView.ViewHolder{
 
     //declare as global variable for codes later
     public TextView name;
+    public TextView name7;
     public TextView description;
+    public TextView description7;
     public View view;
     public View pfp;
 
@@ -18,7 +20,10 @@ public class UserViewHolder extends RecyclerView.ViewHolder{
         super(itemView);
         pfp = itemView.findViewById(R.id.roundLogo);
         name = itemView.findViewById(R.id.txtName);
+        name7 = itemView.findViewById(R.id.txtName2);
         description = itemView.findViewById(R.id.txtDescription);
+        description7 = itemView.findViewById(R.id.txtDescription2);
+
         view = itemView;
     }
 }
