3cc89197e6dcba04028e4c42d7573db4cdb790cf
verrgod <lightsmitevernon@gmail.com>
2021-05-18 21:34:20 +0800

    Complete practical 4 because Android Studio is deleting my lines of code

--- app/src/main/java/sg/edu/np/mad/practical2/UsersAdapter.java
+++ app/src/main/java/sg/edu/np/mad/practical2/UsersAdapter.java
@@ -26,7 +26,13 @@ public class UsersAdapter extends RecyclerView.Adapter<UserViewHolder> {
 
     @Override
     public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.vh_user, parent, false);
+        View item;
+        if (viewType == 0) {
+            item = LayoutInflater.from(parent.getContext()).inflate(R.layout.vh_logo, parent, false);
+        }
+        else {
+            item = LayoutInflater.from(parent.getContext()).inflate(R.layout.vh_user, parent, false);
+        }
         return new UserViewHolder(item);
     }
 
@@ -35,37 +41,79 @@ public class UsersAdapter extends RecyclerView.Adapter<UserViewHolder> {
     public void onBindViewHolder(UserViewHolder holder, int position) {
 
         User u = data.get(position);
-        holder.name.setText(u.getName());
-        holder.description.setText(u.getDescription());
-        holder.view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                androidx.appcompat.app.AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                builder.setTitle("Profile")
-                        .setMessage(u.getName())
-                        .setPositiveButton("VIEW", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Intent profile = new Intent(context, MainActivity.class);
-                                profile.putExtra("Name", u.getName());
-                                profile.putExtra("Description", u.getDescription());
-                                context.startActivity(profile);
-                            }
-                        })
-                        .setNegativeButton("CLOSE", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
 
-                            }
-                        });
-                AlertDialog alertDialog = builder.create();
-                alertDialog.show();
-            }
-        });
+        if (holder.getItemViewType() == 0) {
+            holder.name7.setText(u.getName());
+            holder.description7.setText(u.getDescription());
+            holder.view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    androidx.appcompat.app.AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setTitle("Profile")
+                            .setMessage(u.getName())
+                            .setPositiveButton("VIEW", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent profile = new Intent(context, MainActivity.class);
+                                    profile.putExtra("Name", u.getName());
+                                    profile.putExtra("Description", u.getDescription());
+                                    context.startActivity(profile);
+                                }
+                            })
+                            .setNegativeButton("CLOSE", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                }
+            });
+        }
+        else {
+            holder.name.setText(u.getName());
+            holder.description.setText(u.getDescription());
+            holder.view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    androidx.appcompat.app.AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setTitle("Profile")
+                            .setMessage(u.getName())
+                            .setPositiveButton("VIEW", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent profile = new Intent(context, MainActivity.class);
+                                    profile.putExtra("Name", u.getName());
+                                    profile.putExtra("Description", u.getDescription());
+                                    context.startActivity(profile);
+                                }
+                            })
+                            .setNegativeButton("CLOSE", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                }
+            });
+        }
     }
 
     @Override
     public int getItemCount() {
         return data.size();
+    }
+
+    public int getItemViewType(int position) {
+        User u = data.get(position);
+        if (u.getName().endsWith("7")) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
     }
 }
